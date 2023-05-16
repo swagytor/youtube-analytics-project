ed11758ef053c519a6a2139f17903fd7b1d92f29
Игнатенко Алексей <swagytor@yandex.ru>
2023-05-16 23:56:13 +0300

    added API-key and created YouTube-object

--- src/channel.py
+++ src/channel.py
@@ -1,4 +1,12 @@
-from helper.youtube_api_manual import youtube, printj
+import os
+
+from pprint import pprint
+from dotenv import load_dotenv, find_dotenv
+from googleapiclient.discovery import build
+
+load_dotenv(find_dotenv())
+YT_API_KEY = os.getenv('YT_API_KEY')
+youtube = build('youtube', 'v3', developerKey=YT_API_KEY)
 
 
 class Channel:
@@ -11,4 +19,4 @@ class Channel:
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        printj(channel)
+        pprint(channel)
